74ab1c7ddfba588b749f7198f613ea13d7e20471
Dongwoo Lee <ldw@Dongwoos-MacBook-Pro.local>
2017-01-07 01:37:10 +0900

    add a solution to 2-1 removing duplicated elements in a linked list

---
+++ interviewAnswers/2_1_duplicatedLinkedList/duplicatedLinkedList.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void removeDuplicated(std::list<int>& aList)
+{
+	std:vector<int> checkBuffer;
+	for (std::list<int>::iterator it = aList.begin(); it != aList.end(); ) {
+		int item = *it;
+		if (std::find(checkBuffer.begin(), checkBuffer.end(), item) != checkBuffer.end()) {
+			it = aList.erase(it);
+		} else {
+			checkBuffer.push_back(item);
+			++it;
+		}
+	}
+}
+
+int main()
+{
+
+	std::list<int> sampleLinkedList = {2, 4, 5, 3, 6, 7, 5, 3, 3, 1, 6, 7, 8, 6, 4};
+	removeDuplicated(sampleLinkedList);	
+
+	for (auto item : sampleLinkedList)
+	{
+		std::cout << item << "\n";
+	}
+	
+	return 0;
+}
